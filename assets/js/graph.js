284bb4cfafc1e6e50f362ecf1592d7b861c219ac
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-11-29 12:28:35 -0800

    refactor layoutOptions

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -41,50 +41,49 @@ const initialiseGraph = (data) => {
     }
   });
   
-  var optionBreadthFirst = {
-    name: 'breadthfirst',
-    fit: true,
-    padding: 50,
-    directed: false,
-    spacingFactor: 1.75,
-    avoidOverlap: true
+  var layoutOptions = {
+    breadthFirst: {
+      name: 'breadthfirst',
+      fit: true,
+      padding: 50,
+      directed: false,
+      spacingFactor: 1.75,
+      avoidOverlap: true
+    },
+    circle: {
+      name: 'circle',
+      fit: true,
+      padding: 50,
+      radius: 300,
+      avoidOverlap: true,
+      spacingFactor: 1
+    },
+    cose: {
+      name: 'cose',
+      fit: true,
+      padding: 50,
+      nodeOverlap: 20,
+      componentSpacing: 20,
+      refresh: 20,
+      idealEdgeLength: 100,
+      edgeElasticity: 100,
+      nodeRepulsion: 400000,
+      nestingFactor: 5,
+      gravity: 80,
+      numIter: 1000,
+      initialTemp: 200,
+      coolingFactor: 0.95,
+      minTemp: 1.0,
+      randomize: false
+    },
+    random: {
+      name: 'random',
+      fit: true,
+      padding: 50
+    }
   }
 
-  var optionCircle = {
-    name: 'circle',
-    fit: true,
-    padding: 50,
-    radius: 300,
-    avoidOverlap: true,
-    spacingFactor: 1
-  }
-
-  var optionCose = {
-    name: 'cose',
-    fit: true,
-    padding: 50,
-    nodeOverlap: 20,
-    componentSpacing: 20,
-    refresh: 20,
-    idealEdgeLength: 100,
-    edgeElasticity: 100,
-    nodeRepulsion: 400000,
-    nestingFactor: 5,
-    gravity: 80,
-    numIter: 1000,
-    initialTemp: 200,
-    coolingFactor: 0.95,
-    minTemp: 1.0,
-    randomize: false
-  }
-
-  var optionRandom = {
-    name: 'random',
-    fit: true,
-    padding: 50
-  }
-
-  var layout = cy.layout(optionCose);
+  var layout = cy.layout(layoutOptions.cose);
 
   layout.run();
 }

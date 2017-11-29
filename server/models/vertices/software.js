e37b35f382d82c33d9c3d0756022e92dc80f0643
Gordon <godronus@yahoo.com>
2017-11-29 13:57:17 -0800

    model changes

--- server/models/vertices/software.js
+++ server/models/vertices/software.js
@@ -2,10 +2,14 @@ const g = require('./../../db');
 
 const Software = g.define('software', {
   name: {
-    type: g.STRING
+    type: g.STRING,
+    required: true
   },
   type: {
     type: g.STRING
+  },
+  frontEnd: {
+    type: g.BOOLEAN
   }
 });
 
